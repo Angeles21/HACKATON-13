80537ed5a507e122a4fbbabc71ecef26fbd6d3f6
Angeles21 <pangeles218@gmail.com>
2021-07-03 13:36:56 -0500

    changes

---
+++ app.js
@@ -0,0 +1,35 @@
+/* MAIN */
+const xhr = new XMLHttpRequest();
+
+xhr.open("GET", "./database.json", true);
+
+xhr.onload = function () {
+	const dogo = JSON.parse(this.responseText);
+	console.log(dogo);
+
+	let template = "";
+	dogo.forEach((dogolist) => {
+		template += `
+    <div class="col-sm-6 col-md-3 mb-3">
+      <div class="card" id="demo">
+        <div class="card-body">
+          <button  onclick="myFunction()" href="#" class = "delete"  id="delete"> x </button>
+          <img src="${dogolist.image}" class="dogopic" />
+          <div>
+          <button href="#" class= "edit editdog"  data-id="${dogolist.id}"> Edit </button>
+          <h5 class="card-title">${dogolist.name}</h5>
+          <p class="card-text mb-0 phone"><span class="fw-bolder text-muted"> Phone: </span>${dogolist.phone}</p>
+          <p class="card-text mb-0 email"><span class="fw-bolder text-muted"> Email: </span>${dogolist.email}</p>
+          <hr>
+          <p class="card-text mb-0 about"><span class="fw-bolder text-muted"> About: </span>${dogolist.about}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+	});
+
+	document.getElementById("doglist").innerHTML = template;
+};
+
+xhr.send();
